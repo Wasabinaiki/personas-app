734ea383766d73985f6c5a062aa535eb47eb5be0
Daniel Truque <wasabinaiki@gmail.com>
2024-10-06 00:13:27 -0500

    Commune Edit

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -28,7 +28,10 @@ class ComunaController extends Controller
      */
     public function create()
     {
-        //
+        $municipios = DB::table('tb_municipio')
+        ->orderBy('muni_nomb')
+        ->get();
+        return view('comuna.new', ['municipios' => $municipios]);
     }
 
     /**
@@ -36,7 +39,18 @@ class ComunaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $comuna = new Comuna();
+        // $comuna->comu codi = $request->id;
+        // EI código de comuna es auto incremental
+        $comuna->comu_nomb = $request->name;
+        $comuna->muni_codi = $request->code;
+        $comuna->save();
+        
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio',  'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*', "tb_municipio.muni_nomb")
+        ->get();
+        return view('comuna.index', ['comunas' => $comunas]);
     }
 
     /**
@@ -50,24 +64,49 @@ class ComunaController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit($id)
     {
         //
+        $comuna = Comuna::find($id);
+        $municipios = DB::table('tb_municipio')
+        ->orderBy('muni_nomb')
+        ->get();
+        return view('comuna.edit', ['comuna' => $comuna, 'municipios' => $municipios]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $comuna = Comuna::find($id);
+
+        $comuna->comu_nomb = $request->name;
+        $comuna->muni_codi = $request->code;
+        $comuna->save();
+
+        $comuna = DB::table('tb_comuna')
+        ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*', "tb_municipio.muni_nomb")
+        ->get();
+
+        return view('comuna.index', ['comunas' => $comunas]);
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $comuna = Comuna::find($id);
+        $comuna->delete();
+
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*', "tb_municipio.muni_nomb")
+        ->get();
+
+        return view('comuna.index', ['comunas' => $comunas]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
-<?php
+    <?php
 
-use App\Http\Controllers\ComunaController;
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\ComunaController;
+    use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', function () {
+        return view('welcome');
+    });
 
-Route::get('/comunas', [ComunaController::class, 'index']);
-Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
+    Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas.index');
+    Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
+    Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
+    Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
+    Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
+    Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');
+    Route::get('/comunas/{comuna}/edit', [ComunaController::class, 'edit'])->name('comunas.edit');
+    Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
+    
