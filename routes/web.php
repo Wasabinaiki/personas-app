81c6c7ad5169b3936eccf4b268ee1e4cf0c5cad6
Daniel Truque <wasabinaiki@gmail.com>
2024-10-06 00:54:46 -0500

    Index Pais

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,5 @@
     Route::get('/departamentos/{departamento}/edit', [DepartamentoController::class, 'edit'])->name('departamentos.edit');
     Route::put('/departamentos/{departamento}', [DepartamentoController::class, 'update'])->name('departamentos.update');
     Route::delete('/departamentos/{departamento}', [DepartamentoController::class, 'destroy'])->name('departamentos.destroy');
+
+    Route::get('/paises', [PaisController::class, 'index'])->name('paises.index');
