feac7284ac2baf1da1006cbcb312adab7fef2388
Daniel Truque <wasabinaiki@gmail.com>
2024-10-05 21:53:48 -0500

    Comuna List

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/comunas', [ComunaController::class, 'index']);
+Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
